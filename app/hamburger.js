8be09495724d19a0980bb9436ec39d3f0aa1d75a
buljicS <stefanbvts@gmail.com>
2023-02-10 15:43:19 +0100

    d5

---
+++ app/hamburger.js
@@ -0,0 +1,13 @@
+function hamburger() {
+    var icon = document.getElementById("nav");
+
+    if (icon.style.display === "block") 
+    {
+      icon.style.display = "none";
+    } 
+    
+    else 
+    {
+      icon.style.display = "block";
+    }
+  } 
