d60d1df9367cb6a0d398546809c188ad445dc189
buljicS <stefanbvts@gmail.com>
2023-02-12 20:56:58 +0100

    update

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+// #region-swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+//#endregion-swiper
+
+
