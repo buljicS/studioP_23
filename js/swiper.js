0fbd369fc35d9f3d725b1275e4bd2bd54894df3c
buljicS <stefanbvts@gmail.com>
2023-02-13 14:55:58 +0100

    update

--- js/swiper.js
+++ js/swiper.js
@@ -12,6 +12,25 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
 //#endregion-swiper
 
+//#region swipercard
+var swiperComment = new Swiper(".swipeComments", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+  rotate: 0,
+  stretch: 0,
+  depth: 100,
+  modifier: 2,
+},
 
+navigation: {
+  nextEl: "#rightClick",
+  prevEl: "#leftClick",
+},
+});
+//#endregion swipercard
